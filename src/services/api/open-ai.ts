55738576bbef26c74a18437a61bfa360907e53a1
Himanshu <hpal7860@gmail.com>
2025-09-30 20:56:46 +0400

    fix: hot-fix

--- src/services/api/open-ai.ts
+++ src/services/api/open-ai.ts
@@ -1,28 +1,10 @@
 import { HttpService } from './api-services';
 import API_URLS from './api-urls';
-import { OPEN_AI_SERVICE } from './constant';
-
-const SYSTEM_PROMPT = `Act as a helpful assistant for social support applications. Your role is to help users describe their financial or personal situation accurately, clearly, and empathetically.
-
-You must **only** respond to messages related to the user's application and personal situation (e.g., financial hardship, employment status, etc.).
-
-If a user asks something irrelevant (e.g., stock prices, jokes, general chit-chat), politely redirect them by saying:
-
-"I'm here to help you describe your situation for the social support application. Let’s stay focused so we can best assist you."
-
-Keep your tone professional, friendly, and respectful.`;
-
-const CONTEXT_CLASSIFIER_PROMPT = `Act as a classifier to classify the given context. If the given context is related to the user\'s application and personal situation return "true". If the given context is not related to the user\'s application and personal situation or the prompt looks like "I am poor, how to buy iphone" consider these kind of conversations off topic or personal chitchat, return "false". Moreover make
-          the response more concise and to the point. with below checkpoints
-          
-      - "true" → ONLY if it describes a **severe financial crisis** that impacts the user’s **basic survival** or their **dependents' wellbeing** (e.g., homelessness, inability to afford food, rent, medical care, or other essential needs)
-      -Genral request to write a paragraph for the user's application and personal situation return "true".
-
-      - "false" → For all other messages, including:
-      - General financial worries (like debt or budgeting help)
-      - Emotional struggles without clear financial crisis
-      - Casual conversation, off-topic questions, or small talk
-          `;
+import {
+  CONTEXT_CLASSIFIER_PROMPT,
+  OPEN_AI_SERVICE,
+  SYSTEM_PROMPT,
+} from './constant';
 
 export const OPENAISERVICE = {
   generateText: async (prompt: string, context?: string) => {
@@ -53,7 +35,7 @@ export const OPENAISERVICE = {
     }
   },
 
-  contextCLassifier: async (context: string) => {
+  contextClassifier: async (context: string) => {
     const messages = [
       {
         role: OPEN_AI_SERVICE.SYSTEM_ROLE,
